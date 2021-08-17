c8adb046c0a88204bed8886c26ad4f9898db0601
FrostSue <81924094+FrostSue@users.noreply.github.com>
2021-08-17 17:11:24 +0300

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -32,7 +32,7 @@ async def help(event):
         temp = sorted(temp.split())
         for i in temp:
             final += "`" + str(i)
-            final += "`\n\t\t•\t\t\t\n"
+            final += "`\n\t\t•\t\t\t"
         msg=await event.edit(f"{final[:-5]}")
         await sleep(45)
         
